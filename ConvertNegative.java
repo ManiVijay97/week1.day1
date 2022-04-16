d22ddbf134f2818fdd3099aa0aac5609f901bd03
ManiVijay97 <103831888+ManiVijay97@users.noreply.github.com>
2022-04-16 20:56:20 +0530

    Add files via upload

---
+++ ConvertNegative.java
@@ -0,0 +1,15 @@
+package week1.day1.assignments;
+
+public class ConvertNegative {
+	public static void main(String[] args) {
+		
+		int a=-40;
+		int b;
+		if (a<0)
+		{
+			b = a * -1;
+			System.out.println("The number " + a + " is converted to " + b);
+		}
+	}
+
+}
---
+++ NegativeOrPositiveNumber.java
@@ -0,0 +1,19 @@
+package week1.day1.assignments;
+
+public class NegativeOrPositiveNumber {
+public static void main(String[] args) {
+	int a = 0;
+	if (a < 0)
+		System.out.println("The number is negative"); 
+	else 
+	{
+		if (a > 0)
+			System.out.println("The number is positive");
+		else
+			System.out.println("The number is neither positive nor negative");
+			
+			
+	}
+	
+}
+}
